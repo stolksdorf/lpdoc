22a9913ff4dc23fc4edbc15b0a6ee1d0e9eab112
Scott Tolksdorf <scott.tolksdorf@gmail.com>
2014-12-11 19:39:30 -0500

    Shrinking it down even further

--- server.js
+++ server.js
@@ -1,12 +1,7 @@
 var express = require("express");
-//var recoil = require('recoil');
 var app = express();
 var fs = require('fs');
-require('node-jsx').install();
 app.use(express.static(__dirname + '/build'));
-
-
-
 
 app.get("*", function (req, res) {
 	return res.send(fs.readFileSync('./build/lpdoc/index.html', 'utf8'));
